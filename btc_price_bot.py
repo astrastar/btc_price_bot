859a92035583e74e2120c2e1e9000d777cdf2a14
astrastar <37809311+astrastar@users.noreply.github.com>
2018-04-19 06:35:19 -0400

    Create btc_price_bot.py

---
+++ btc_price_bot.py
@@ -0,0 +1,14 @@
+from coins import get_price
+import telebot
+
+
+TOKEN = '575819178:AAEIDmSjlvLw9XINaUcn16iy9EQwj6R48-4'
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['price_btc'])
+def print_price(message):
+    bot.reply_to(message, get_price())
+
+bot.polling(timeout=60)
